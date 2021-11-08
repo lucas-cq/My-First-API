51ba04afebd4242764e87bb27947211a9ab93818
Lucas Cummings <lucas.cummings024@gmail.com>
2021-11-08 13:27:55 -0700

    Added mongo connection, now place it in seeds

--- server.js
+++ server.js
@@ -1,40 +1,60 @@
 // Install modules
+const { request } = require('express')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const { stringify } = require('qs')
 const dotenv = require('dotenv').config()
 
-const cars = require('./models/gallery.js')
+//const cars = require('./models/gallery.js')
 
+/* ******************************* */
+/* THIS WILL GO INTO MODEL FOLDER! */
+/* ******************************* */
 
-// Endpoint for the API
-app.get('/api/cars', (req, res) => {
-  res.send(cars)
-
-  if (typeof cars !== 'undefined' && Array.isArray(cars)) {
-    res.send(cars)
-  } else {
-    res.status(404)
-    res.send({error: '404: File Not Found'})
-  }
-
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+)
+.then(() => {
+  console.log('Connected to DB...')
+})
+.catch((err) => {
+  console.log(err)
 })
 
-app.get('/api/cars/:id', (req, res) => {
-  let galleryItem
+const gallerySchema = new mongoose.Schema({
+  id: Number,
+  title: String,
+  description: String,
+  pathURL: String,
+  linkURL: String,
+  credit: String,
+  creditURL: String,
+  date: String
+})
 
-  if (typeof cars !== 'undefined' && Array.isArray(cars)) {
-    galleryItem = cars.find(item => Number(req.params.id) === item.id)
-  } else {
-    galleryItem = null
+const Car = mongoose.model('Car', gallerySchema)
+
+// Endpoint for the API
+app.get('/api/cars', async (req, res) => {
+  try {
+  const cars = await Car.find()
+  res.send(cars)
+  } catch(err) {
+    res.send({error: '404 File not found'})
   }
+})
 
-   // Use Array.find() here
-  if (typeof galleryItem === 'object' && galleryItem !== null) {
-    res.send(galleryItem)
-  } else {
-    res.status(404)
-    res.send({error: '404: File Not Found'})
+app.get('/api/cars/:id', async (req, res) => {
+  try {
+  const car = await Car.findOne({id: req.params.id})
+  if (!car) {
+    throw new Error('Car not found')
+  }
+  res.send(car)
+  } catch(err) {
+    res.send({error: '404 Car not found'})
   }
 })
 
